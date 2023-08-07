a18bedfde313183dc13ce95d2611a0b6600c3835
pafz <paferza@gmail.com>
2023-08-07 13:36:31 +0200

    first commit

---
+++ src/components/CamposDeTexto/CamposDeTexto.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import './CamposDeTexto.css';
+export const CamposDeTexto = ({ state, className, line = 'line-1.svg' }) => {
+  return (
+    <div className={`campos-de-texto ${className}`}>
+      <div className={`group ${state}`}>
+        <div className="overlap-group">
+          {['active-web', 'default', 'error'].includes(state) && (
+            <div className="text-wrapper">Ej. realtor@valencia.com</div>
+          )}
+          {state === 'focus-web' && (
+            <img className="line" alt="Line" src={line} />
+          )}
+        </div>
+        <div className="div">Correo electrónico</div>
+      </div>
+    </div>
+  );
+};
+CamposDeTexto.propTypes = {
+  state: PropTypes.oneOf(['active-web', 'error', 'focus-web', 'default']),
+  line: PropTypes.string,
+};
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import './Logo.css';
+// import settingImage from './../../../public/assets';
+// import image from './../../../public/assets';
+
+export const Logo = ({ size, className, setting = 'setting-1-2.svg' }) => {
+  return (
+    <div className={`logo ${size} ${className}`}>
+      <img
+        className="setting"
+        alt="Setting"
+        src={size === 'big' ? settingImage : size === 'small' ? image : setting}
+      />
+      <div className="text-wrapper">RealtorVLC1.0</div>
+    </div>
+  );
+};
+Logo.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'big']),
+  setting: PropTypes.string,
+};
